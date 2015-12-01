11d529c07cfcf7bebda9809c93a677e120193d9a
rdrm2014 <ricardomendes@staff-0587.wireless.ua.pt>
2015-12-01 13:44:56 +0000

    Change nodes

--- red/api/flows.js
+++ red/api/flows.js
@@ -24,42 +24,44 @@ var Nodes = require("../../app/models/nodes");
 var client = mqtt.connect("mqtt://192.168.160.122");
 
 module.exports = {
-    get: function(req,res) {
-        Nodes.findOne({'user_id': req.user._id}, function(err, node) {
+    get: function (req, res) {
+        Nodes.findOne({'user_id': req.user._id}, function (err, node) {
             if (err) return handleError(err);
-            if(node)
+            if (node)
                 res.json(JSON.parse(node.nodes));
             else
                 res.json([]);
         });
     },
-    post: function(req,res) {
+    post: function (req, res) {
         var flows = req.body;
-        var deploymentType = req.get("Node-RED-Deployment-Type")||"full";
-        log.audit({event: "flows.set",type:deploymentType},req);
+        var deploymentType = req.get("Node-RED-Deployment-Type") || "full";
+        log.audit({event: "flows.set", type: deploymentType}, req);
 
         flows = getJson(flows);
 
-        redNodes.setFlows(flows,deploymentType).then(function() {
+        redNodes.setFlows(flows, deploymentType).then(function () {
             res.send(204);
-        }).otherwise(function(err) {
-            log.warn(log._("api.flows.error-save",{message:err.message}));
+        }).otherwise(function (err) {
+            log.warn(log._("api.flows.error-save", {message: err.message}));
             log.warn(err.stack);
-            res.json(500,{error:"unexpected_error", message:err.message});
+            res.json(500, {error: "unexpected_error", message: err.message});
         });
 
-        Nodes.findOne({'user_id': req.user._id}, function(err, node) {
+        Nodes.findOne({'user_id': req.user._id}, function (err, node) {
             if (!node) {
                 var nodeElement = new Nodes({user_id: req.user._id, nodes: [JSON.stringify(flows)]});
                 nodeElement.save(function (err) {
                     if (err) return handleError(err);
                 })
+            } else {
+                node.nodes = [JSON.stringify(flows)];
+
+                node.save(function (err) {
+                    if (err) return handleError(err);
+                })
             }
-            node.nodes = [JSON.stringify(flows)];
-            node.save(function (err) {
-                if (err) return handleError(err);
-            })
-            });
+        });
     }
 };
 
@@ -68,7 +70,7 @@ function getJson(flows) {
     var x1, x2, x3;
     for (var i = 0; i < flows.length; i++) {
         if (flows[i].type !== 'tab') {
-            for (var z = i+1; z < flows.length; z++) {
+            for (var z = i + 1; z < flows.length; z++) {
                 for (var x = flows.length - 1; x > z; x--) {
                     if (flows[i].wires.length) {
                         if (
@@ -119,7 +121,7 @@ function getJson(flows) {
                     }
                 }
             }
-            if(x1 && x2 && x3) {
+            if (x1 && x2 && x3) {
                 rules.push({x1: x1, x2: x2, x3: x3});
                 x1 = x2 = x3 = undefined;
 
@@ -134,36 +136,50 @@ function formuleRules(rules, flows) {
         var x1 = rules[i].x1, x2 = rules[i].x2, x3 = rules[i].x3;
         var rule3 = x3.rule.func = x1.rule.func = x2.rule.func = '';
         x1.rule.func = x2.rule.type + ' ' + x1.rule.type;
-        switch(x1.rule.type) {
+        switch (x1.rule.type) {
             case 'Temperature':
-                x2.rule.func = flows[x2.index].func = 'temp : Temperature(getReadState() == true, currentTemp' +
-                    setConditionRules(x2.rule);
-                rule3 = x3.rule.on_or_off ? 'temp.setReadState(false); temp.setRelayON(); update(temp);' :
-                    'temp.setReadState(false); temp.setRelayOff(); update(temp);';
-
+                x2.rule.func = flows[x2.index].func = 'temp : Temperature(currentTemp' + setConditionRules(x2.rule);
+                switch (x3.rule.type) {
+                    case 'Relay':
+                        rule3 = x3.rule.on_or_off ? 'temp.setRelayON();' : ' temp.setRelayOFF();';
+                        break;
+                    case 'IR':
+                        rule3 = 'temp.getIR().sendCommand(\"' + x3.rule.command + '\");';
+                        break;
+                }
                 break;
             case 'Pir':
-                x1.rule.func = "Movement detector";
-                x2.rule.func = 'pir : PIRSensor( getCurrentState() == ' + (x2.rule.value ? true : false) + ')';
-                rule3 = 'pir.setCurrentState(false); System.out.println("PIRSensor rule: Movement detected!");';
+                //x1.rule.func = "Movement detector";
+                x2.rule.func = 'pir : PIRSensor( getCurrentState() == ' + (x2.rule.value=="1" ? true : false) + ')';
+                switch (x3.rule.type) {
+                    case 'Relay':
+                        rule3 = x3.rule.on_or_off ? 'pir.setRelayON();' : ' pir.setRelayOFF();';
+                        break;
+                    case 'IR':
+                        rule3 = 'pir.getIR().sendCommand(\"' + x3.rule.command + '\");';
+                        break;
+                }
                 break;
         }
 
         var d = new Date();
 
         if (x3.rule.sms_checkbox) {
-            x3.rule.func = 'Notification.sendSMS("'+x3.rule.sms+'", "'+ d.toISOString()+'", "'+x3.rule.message+'", "'+x3.rule.id+'");';
+            x3.rule.func = 'Notification.sendSMS("' + x3.rule.sms + '", "' + d.toISOString() + '", "' + x3.rule.message + '", "' + x3.rule.id + '");';
         }
         if (x3.rule.whats_app_checkbox) {
-            x3.rule.func =  x3.rule.func + 'Notification.sendWhatsApp("+351'+x3.rule.sms+'", "'+d.toISOString()+'", "'+x3.rule.message+'", "'+x3.rule.id+'");';
+            x3.rule.func = x3.rule.func + 'Notification.sendWhatsApp("351' + x3.rule.sms + '", "' + d.toISOString() + '", "' + x3.rule.message + '", "' + x3.rule.id + '");';
         }
         if (x3.rule.email_checkbox) {
-            x3.rule.func = x3.rule.func + 'Notification.sendEmail("'+x3.rule.email+'", "'+d.toISOString()+'", "IESI", "'+x3.rule.message+'", "'+x3.rule.id+'");';
+            x3.rule.func = x3.rule.func + 'Notification.sendEmail("' + x3.rule.email + '", "' + d.toISOString() + '", "IESI", "' + x3.rule.message + '", "' + x3.rule.id + '");';
         }
 
         x3.rule.func += rule3;
 
-        client.publish('MessageBroker', new Buffer(JSON.stringify({name: x1.rule.func, data : [x1.rule, x2.rule, x3.rule]})));
+        client.publish('MessageBroker', new Buffer(JSON.stringify({
+            name: x1.rule.name,
+            data: [x1.rule, x2.rule, x3.rule]
+        })));
     }
 
     return flows;
@@ -173,19 +189,19 @@ function setConditionRules(rule) {
     var value = '';
     switch (rule.type) {
         case 'Max':
-            value = ' > '+ rule.value + ' )';
+            value = ' > ' + rule.value + ' )';
             break;
         case 'Min':
-            value = ' < '+ rule.value + ' )';
+            value = ' < ' + rule.value + ' )';
             break;
         case 'Equals':
-            value = ' == '+ rule.value + ' )';
+            value = ' == ' + rule.value + ' )';
             break;
         case 'Max or equals':
-            value = ' >= '+ rule.value + ' )';
+            value = ' >= ' + rule.value + ' )';
             break;
         case 'Min or equals':
-            value = ' <= '+ rule.value + ' )';
+            value = ' <= ' + rule.value + ' )';
             break;
         default:
             break;
